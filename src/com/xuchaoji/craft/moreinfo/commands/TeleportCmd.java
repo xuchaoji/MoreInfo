21dc34fe0686b22968f644aef6f80e601187227b
xuchaoji <admin@xuchaoji.com>
2021-11-07 19:16:05 +0800

    [20211107]diamond for teleport point

--- src/com/xuchaoji/craft/moreinfo/commands/TeleportCmd.java
+++ src/com/xuchaoji/craft/moreinfo/commands/TeleportCmd.java
@@ -6,10 +6,12 @@ import com.xuchaoji.craft.moreinfo.util.ConfigHelper;
 import com.xuchaoji.craft.moreinfo.util.MsgSender;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TeleportCmd implements CommandExecutor {
@@ -38,14 +40,16 @@ public class TeleportCmd implements CommandExecutor {
         Location location = player.getLocation();
         msgSender.sendConsoleMsg("location: " + CommonUtil.getLocStr(location));
         if (args == null || args.length == 0) {
-            msgSender.sendPlayerMsg(player, "/ddtp add name 添加名为name的传送点。", ChatColor.GREEN);
-            msgSender.sendPlayerMsg(player, "/ddtp goto name 传送到名为name的传送点。", ChatColor.GREEN);
-            msgSender.sendPlayerMsg(player, "/ddtp list 查看传送点列表。", ChatColor.GREEN);
+            pluginMsg(player);
             return true;
         }
         switch (args[0]) {
             case ADD:
-                addLocation(args, player, location);
+                if (canCreateTpPoint(player)) {
+                    addLocation(args, player, location);
+                } else {
+                    pluginMsg(player);
+                }
                 break;
             case LIST:
                 String locList = configHelper.getLocList(player);
@@ -56,6 +60,21 @@ public class TeleportCmd implements CommandExecutor {
                 break;
             default:
                 onParamError(player);
+        }
+        return true;
+    }
+
+    private void pluginMsg(Player player) {
+        msgSender.sendPlayerMsg(player, "/ddtp add name 添加名为name的传送点。", ChatColor.GREEN);
+        msgSender.sendPlayerMsg(player, "/ddtp goto name 传送到名为name的传送点。", ChatColor.GREEN);
+        msgSender.sendPlayerMsg(player, "/ddtp list 查看传送点列表。", ChatColor.GREEN);
+        msgSender.sendPlayerMsg(player, "传送点需要手持钻石创建，一颗钻石可传送一次。", ChatColor.YELLOW);
+    }
+
+    private boolean canCreateTpPoint(Player player) {
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        if (null == itemStack || itemStack.getType() != Material.DIAMOND) {
+            return false;
         }
         return true;
     }
@@ -89,7 +108,13 @@ public class TeleportCmd implements CommandExecutor {
         if (args.length < 2) {
             msgSender.sendPlayerMsg(player, "参数错误.", ChatColor.RED);
         }
-        DDLocation ddLocation = new DDLocation(args[1], location, player, false, 20);
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        if (itemStack == null || itemStack.getType() != Material.DIAMOND) {
+            msgSender.sendPlayerMsg(player, "传送点需要手持钻石创建，一颗钻石可传送一次。", ChatColor.YELLOW);
+            return;
+        }
+        DDLocation ddLocation = new DDLocation(args[1], location, player, false, itemStack.getAmount());
+        player.getInventory().setItemInMainHand(null);
         configHelper.saveDDLocation(ddLocation);
         msgSender.sendPlayerMsg(player, "传送点: " + ddLocation.getName() + ", 创建成功，剩余传送次数： " + ddLocation.getRemainTimes());
     }
